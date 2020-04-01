e901267823797290fdb1837c9ab9086958ebd653
SiletFlower <747671555@qq.com>
2020-04-02 03:38:39 +0800

    链表复习(头插法和尾插法) 2020年4月2日03:37:46

---
+++ src/main/java/List/SingleList.java
@@ -0,0 +1,91 @@
+package List;
+
+import lombok.Data;
+
+/**
+ * @author SiletFlower
+ * @date 2020/3/31 21:03:34
+ * @description
+ */
+
+/**
+* 单链表的创建
+ * 1.定义Node类，设置data成员变量放数据，next节点存放下一个节点
+ * 2.创建单链表的类，创建头和尾节点用来存放头和尾。
+ * 3.1.通过头插法创建链表，方式为当head为null将newNode的对象地址给head,如果head已经有了对象地址，那么将新的节点指向
+ * 头节点(此时头节点是第一个节点的对象地址，在把新的节点对象给head)，这样就形成了先进后出的栈形式的链表
+ * 3.2.通过尾插法创建链表，方式为使用tailer来记录最新节点的位置。方法为设置最后一个节点对象地址的tailer的下一个地址为
+ * 新的节点的对象，然后将tailer传入新的节点的对象。
+ *
+ * 遍历的方法为设置一个node对象，将head对象地址给它，使用while判断在node不为空的情况下，将node的数据打印并，设置node为
+ * next node。
+* */
+public class SingleList {
+    @Data
+    class Node{
+        private int data;
+        private Node next;
+        public Node(int data){this.data=data;}
+    }
+
+    private Node head;
+    private Node tailer;
+    private int length;
+    public SingleList(){this.head=null;}
+    //尾插法，先进先出实现队列
+    public void createList_queue(int data){
+        Node newNode = new Node(data);
+        if (head==null){
+            head=tailer=newNode;
+        }else{
+            tailer.setNext(newNode);
+            tailer=newNode;
+        }
+        length++;
+
+    }
+
+
+    //头插法，先进后出实现栈结构
+    public void createList_stack(int data){
+        Node newNode = new Node(data);
+        if (head==null){
+            head=newNode;
+        }else{
+            newNode.setNext(head);
+            head=newNode;
+        }
+        length++;
+    }
+
+    public void print(SingleList list){
+        Node p = list.head;
+        while(p!=null){
+            System.out.print(p.getData()+" ");
+            p=p.getNext();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        int m = 7;
+        SingleList singleList = new SingleList();
+        while(m-- > 1){
+            singleList.createList_stack(m);
+        }
+        System.out.println("头插法");
+        singleList.print(singleList);
+
+        m = 7;
+        SingleList singleList2 = new SingleList();
+        while(m-- > 1){
+            singleList2.createList_queue(m);
+        }
+        System.out.println();
+        System.out.println("尾插法");
+        singleList.print(singleList2);
+
+    }
+}
+
--- src/main/java/String/Reverse.java
+++ src/main/java/String/Reverse.java
@@ -1,5 +1,8 @@
 package String;
 
+import lombok.Data;
+import org.junit.Test;
+
 /**
  * @author SiletFlower
  * @date 2020/3/31 19:58:45
@@ -50,5 +53,59 @@ public class Reverse {
             s.setCharAt(j--,temp);
         }
         return s;
+    }
+
+
+    /*
+    *
+    * 1、链表翻转。给出一个链表和一个数k，比如，链表为1→2→3→4→5→6，k=2，则翻转后2→1→6→5→4→3，若k=3，翻转后3→2→1→6→5→4，若k=4，翻转后4→3→2→1→6→5，用程序实现。
+      2、编写程序，在原字符串中把字符串尾部的m个字符移动到字符串的头部，要求：长度为n的字符串操作时间复杂度为O(n)，空间复杂度为O(1)。 例如，原字符串为”Ilovebaofeng”，m=7，输出结果为：”baofengIlove”。
+      3、单词翻转。输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变，句子中单词以空格符隔开。为简单起见，标点符号和普通字母一样处理。例如，输入“I am a student.”，则输出“student. a am I”。
+    * */
+
+    @Data
+    class Node{
+        private int data;
+        private Node next;
+        public Node(int data){this.data=data;}
+
+    }
+
+    class NodeList{
+        private Node head;
+        private Node tailer;
+        private int length = 0;
+        public NodeList(){this.head=null;}
+
+        public void addNode(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = tailer= newNode;
+            }else{
+                tailer.setNext(newNode);
+                tailer=newNode;
+            }
+            length++;
+        }
+
+        public void print(){
+            Node p = head;
+            while (p!=null){
+                System.out.print(p.getData()+" ");
+                p=p.next;
+            }
+            System.out.println();
+        }
+
+    }
+
+    @Test
+    public void think01(){
+        NodeList list = new NodeList();
+        int n = 0;
+        while (n++ <6){
+            list.addNode(n);
+        }
+        list.print();
     }
 }
